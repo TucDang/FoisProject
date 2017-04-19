b933dabe69739d22dbaf5358fc3113d04d5bd21d
TucDang <thanhtucit.95@gmail.com>
2017-04-19 17:00:28 +0700

    controller

--- src/main/java/com/example/controllers/HelloController.java
+++ src/main/java/com/example/controllers/HelloController.java
@@ -2,10 +2,6 @@ package com.example.controllers;
 
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
